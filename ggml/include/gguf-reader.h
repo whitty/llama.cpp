f85228c71279644476b9e2618074d06acdfae9a8
Greg Whiteley <greg.whiteley@gmail.com>
2026-06-25 20:25:37 +1000

    ggml: Move non-templated, multi-line gguf-reader function definitions to .cpp

--- ggml/include/gguf-reader.h
+++ ggml/include/gguf-reader.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "ggml.h"
-#include "ggml-impl.h"
 #include "gguf.h"
 
-#include <cinttypes>
 #include <cstddef>
+#include <cstdint>
 #include <cstdio>
 #include <string>
 #include <type_traits>
@@ -40,41 +39,16 @@
 #define GGUF_MAX_STRING_LENGTH  (1024*1024*1024)
 #define GGUF_MAX_ARRAY_ELEMENTS (1024*1024*1024)
 
-struct GGML_API_CLASS gguf_reader {
-    gguf_reader(
-            gguf_reader_callback_t callback,
-            void * userdata,
-            size_t max_chunk_read,
-            uint64_t data_offset = 0,
-            uint64_t nbytes_remain = 0)
-        : callback(callback),
-          userdata(userdata),
-          max_chunk_read(max_chunk_read),
-          data_offset(data_offset),
-          nbytes_remain(nbytes_remain) {
-        GGML_ASSERT(max_chunk_read > 0);
-    }
+struct GGML_API_CLASS gguf_reader
+{
+    gguf_reader(gguf_reader_callback_t callback,
+                void * userdata,
+                size_t max_chunk_read,
+                uint64_t data_offset = 0,
+                uint64_t nbytes_remain = 0);
 
     // helper for remaining bytes in a file
-    static uint64_t file_remain(FILE * file) {
-        const int64_t cur = gguf_ftell(file);
-        if (cur < 0) {
-            return 0;
-        }
-        if (gguf_fseek(file, 0, SEEK_END) != 0) {
-            gguf_fseek(file, cur, SEEK_SET);
-
-            return 0;
-        }
-        const int64_t end = gguf_ftell(file);
-        if (end < 0) {
-            gguf_fseek(file, cur, SEEK_SET);
-
-            return 0;
-        }
-        gguf_fseek(file, cur, SEEK_SET);
-        return static_cast<uint64_t>(end - cur);
-    }
+    static uint64_t file_remain(FILE * file);
 
     template <typename T>
     bool read(T & dst) const {
@@ -123,106 +97,20 @@ struct GGML_API_CLASS gguf_reader {
         return true;
     }
 
-    bool read(bool & dst) const {
-        int8_t tmp = -1;
-        if (!read(tmp)) {
-            return false;
-        }
-        dst = tmp != 0;
-        return true;
-    }
+    bool read(bool & dst) const;
+    bool read(enum ggml_type & dst) const;
+    bool read(enum gguf_type & dst) const;
+    bool read(std::string & dst) const;
+    bool read(void * dst, const size_t size) const;
 
-    bool read(enum ggml_type & dst) const {
-        int32_t tmp = -1;
-        if (!read(tmp)) {
-            return false;
-        }
-        dst = ggml_type(tmp);
-        return true;
-    }
-
-    bool read(enum gguf_type & dst) const {
-        int32_t tmp = -1;
-        if (!read(tmp)) {
-            return false;
-        }
-        dst = gguf_type(tmp);
-        return true;
-    }
-
-    bool read(std::string & dst) const {
-        uint64_t size = 0;
-        if (!read(size)) {
-            return false;
-        }
-        if (size > GGUF_MAX_STRING_LENGTH) {
-            GGML_LOG_ERROR("%s: string length %" PRIu64 " exceeds maximum %" PRIu64 "\n", __func__, size, (uint64_t) GGUF_MAX_STRING_LENGTH);
-            return false;
-        }
-        if (size > nbytes_remain) {
-            GGML_LOG_ERROR("%s: string length %" PRIu64 " exceeds remaining file size %" PRIu64 " bytes\n", __func__, size, nbytes_remain);
-            return false;
-        }
-        dst.resize(static_cast<size_t>(size));
-        return read_raw(dst.data(), static_cast<size_t>(size)) == size;
-    }
-
-    bool read(void * dst, const size_t size) const {
-        if (size > nbytes_remain) {
-            return false;
-        }
-        return read_raw(dst, size) == size;
-    }
+    bool seek(uint64_t absolute_offset) const;
 
     uint64_t tell() const {
         return data_offset;
     }
 
-    bool seek(uint64_t absolute_offset) const {
-        const uint64_t end_offset = uint64_t(data_offset) + nbytes_remain;
-        if (absolute_offset > end_offset) {
-            return false;
-        }
-
-        data_offset = absolute_offset;
-        nbytes_remain = end_offset - absolute_offset;
-
-        return true;
-    }
-
 private:
-    size_t read_raw(void * dst, size_t size) const {
-        if (callback == nullptr || size == 0) {
-            return 0;
-        }
-
-        uint8_t * data = static_cast<uint8_t *>(dst);
-        size_t total_nread = 0;
-        bool reached_eof = false;
-
-        while (total_nread < size) {
-            const size_t chunk_size = std::min(max_chunk_read, size - total_nread);
-            if (data_offset + total_nread < data_offset) {
-                break;
-            }
-            const size_t nread = callback(userdata, static_cast<void *>(data + total_nread), data_offset + total_nread, chunk_size);
-            total_nread += nread;
-            if (nread != chunk_size) {
-                reached_eof = true;
-                break;
-            }
-        }
-
-        data_offset += total_nread;
-        GGML_ASSERT(total_nread <= nbytes_remain);
-        nbytes_remain -= total_nread;
-
-        if (reached_eof) {
-            nbytes_remain = 0;
-        }
-
-        return total_nread;
-    }
+    size_t read_raw(void * dst, size_t size) const;
 
     gguf_reader_callback_t callback = nullptr;
     void * userdata = nullptr;
