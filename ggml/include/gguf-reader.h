f63cb75a4c12e090f1a96164ce171b95f481e1e4
Greg Whiteley <greg.whiteley@gmail.com>
2026-07-01 19:38:16 +1000

    ggml: Allow for `gguf_reader` implementation to be overridden

--- ggml/include/gguf-reader.h
+++ ggml/include/gguf-reader.h
@@ -6,6 +6,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
+#include <memory>
 #include <string>
 #include <type_traits>
 #include <vector>
@@ -39,6 +40,45 @@
 #define GGUF_MAX_STRING_LENGTH  (1024*1024*1024)
 #define GGUF_MAX_ARRAY_ELEMENTS (1024*1024*1024)
 
+struct GGML_API_CLASS gguf_reader_impl {
+    gguf_reader_impl(gguf_reader_callback_t callback,
+                     void * userdata,
+                     size_t max_chunk_read,
+                     uint64_t data_offset = 0,
+                     uint64_t nbytes_remain = 0);
+
+    virtual ~gguf_reader_impl() = default;
+
+    virtual size_t read_raw(void * dst, size_t size) const;
+
+    virtual uint64_t remaining() const {
+        return nbytes_remain;
+    }
+
+    virtual bool seek(uint64_t absolute_offset) const;
+
+    virtual uint64_t tell() const {
+        return data_offset;
+    }
+
+private:
+    gguf_reader_callback_t callback = nullptr;
+    void * userdata = nullptr;
+    size_t max_chunk_read = 0;
+    mutable uint64_t data_offset = 0;
+    mutable uint64_t nbytes_remain = 0;
+};
+
+struct GGML_API_CLASS gguf_reader_impl_factory {
+    virtual ~gguf_reader_impl_factory() = default;
+
+    virtual std::unique_ptr<gguf_reader_impl> build_for(gguf_reader_callback_t callback,
+                                                        void * userdata,
+                                                        size_t max_chunk_read,
+                                                        uint64_t data_offset = 0,
+                                                        uint64_t nbytes_remain = 0) = 0;
+};
+
 struct GGML_API_CLASS gguf_reader
 {
     gguf_reader(gguf_reader_callback_t callback,
@@ -53,7 +93,7 @@ struct GGML_API_CLASS gguf_reader
     template <typename T>
     bool read(T & dst) const {
         const size_t size = sizeof(dst);
-        if (size > nbytes_remain) {
+        if (size > impl->remaining()) {
             return false;
         }
         return read_raw(&dst, size) == size;
@@ -69,14 +109,14 @@ struct GGML_API_CLASS gguf_reader
             if (n > SIZE_MAX / sizeof(uint64_t)) {
                 return false;
             }
-            if (nbytes_remain < n * sizeof(uint64_t)) {
+            if (impl->remaining() < n * sizeof(uint64_t)) {
                 return false;
             }
         } else {
             if (n > SIZE_MAX / sizeof(T)) {
                 return false;
             }
-            if (nbytes_remain < n * sizeof(T)) {
+            if (impl->remaining() < n * sizeof(T)) {
                 return false;
             }
         }
@@ -106,15 +146,11 @@ struct GGML_API_CLASS gguf_reader
     bool seek(uint64_t absolute_offset) const;
 
     uint64_t tell() const {
-        return data_offset;
+        return impl->tell();
     }
 
 private:
-    size_t read_raw(void * dst, size_t size) const;
+    virtual size_t read_raw(void * dst, size_t size) const;
 
-    gguf_reader_callback_t callback = nullptr;
-    void * userdata = nullptr;
-    size_t max_chunk_read = 0;
-    mutable uint64_t data_offset = 0;
-    mutable uint64_t nbytes_remain = 0;
+    std::unique_ptr<gguf_reader_impl> impl;
 };
